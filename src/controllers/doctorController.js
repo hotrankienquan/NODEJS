66b7795551ec185f8dbbcfb8b80038642936de45
Kien Quan <hotrankienquan144@gmail.com>
2021-11-11 16:42:57 +0700

    load thong tin bac si 63

---
+++ src/controllers/doctorController.js
@@ -0,0 +1,18 @@
+import doctorService from '../services/doctorService';
+let getTopDoctorHome = async (req, res) => {
+	let limit = req.query.limit;
+	if(!limit) limit = 10; 
+	try {
+		let response = await doctorService.getTopDoctorHome(+limit);// +limit: convert string sang number
+		return res.status(200).json(response)
+	} catch (error) {
+		console.log(error)
+		return res.status(200).json({
+			errCode: -1,
+			message: 'error from server'
+		})
+	}
+}
+module.exports = {
+	getTopDoctorHome: getTopDoctorHome
+}
