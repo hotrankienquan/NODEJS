9037824024456bd319f9a09a286cd9833fb84a09
Kien Quan <hotrankienquan144@gmail.com>
2021-11-27 18:21:20 +0700

    #78 them giao dien luu infor bac si

---
+++ src/migrations/migration-doctor-infor.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('doctor_infor', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			doctorId: {
+				allowNull: false,
+				type: Sequelize.INTEGER
+			},
+			priceId: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			provinceId: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			paymentId: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			addressClinic: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			nameClinic: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			note: {
+				type: Sequelize.STRING
+			},
+			count: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+				defaultValue: 0
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('doctor_infor');
+	}
+};
